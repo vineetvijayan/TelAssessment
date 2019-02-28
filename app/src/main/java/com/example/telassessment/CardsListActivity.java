50c782daa96ebaccb68469c08074685cf13192fd
Vineet Vijayan <vineetvijayan31@gmail.com>
2019-02-28 18:36:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/telassessment/CardsListActivity.java
@@ -0,0 +1,13 @@
+package com.example.telassessment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_cards);
+    }
+}
