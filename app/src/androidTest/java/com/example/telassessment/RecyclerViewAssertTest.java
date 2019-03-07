4e2a74a40084e249ab034af8a469bb938fa79d38
Vineet Vijayan <vineetvijayan31@gmail.com>
2019-03-07 15:49:19 +0530

    Added more UI tests for Recycler view

--- app/src/androidTest/java/com/example/telassessment/RecyclerViewAssertTest.java
+++ app/src/androidTest/java/com/example/telassessment/RecyclerViewAssertTest.java
@@ -1,5 +1,7 @@
 package com.example.telassessment;
 
+import android.support.test.espresso.IdlingRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -11,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -37,5 +40,22 @@ public class RecyclerViewAssertTest {
     public void ensureRecyclerViewIsError() {
         onView(withId(R.id.tv_no_data))
                 .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void ensureRecyclerViewIsScrolling() {
+        onView(withId(R.id.item_list))
+                .perform(RecyclerViewActions.scrollToPosition(5));
+    }
+
+    @Test
+    public void assertRecyclerViewClick() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(withId(R.id.item_list))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
     }
 }
